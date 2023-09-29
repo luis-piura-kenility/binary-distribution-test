cba051a36960cb5fd9a700ac57c73fb45ccb9e97
Fran Piura <luis.piura@kenility.com>
2023-09-29 17:16:32 -0600

    0.0.5

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", .exact("2.33.4"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "TruvideoSdkMedia",
-            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.4/TruvideoSdkMedia.xcframework.zip",
-            checksum: "97e73d538cda2183287a2073313256c57a6c269c847500038ee93f040a0570a3"
+            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.5/TruvideoSdkMedia.xcframework.zip",
+            checksum: "7ab1bc8174f3524246742e105ce7affe10d8f8a960b51b82f99d09ed542c749f"
         ),
         .target(
             name: "TruvideoSdkMediaTargets",
