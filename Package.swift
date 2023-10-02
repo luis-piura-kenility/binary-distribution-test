cde4f490162b505712f702f8570041e2632fbe00
Fran Piura <luis.piura@kenility.com>
2023-10-02 07:02:00 -0600

    0.0.8

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "TruvideoSdkMedia",
+    name: "truvideoSdkMedia",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TruvideoSdkMedia",
+            name: "truvideoSdkMedia",
             targets: ["TruvideoSdkMediaTargets"]),
     ],
     dependencies: [
