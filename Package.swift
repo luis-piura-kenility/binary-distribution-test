3b5f995f759489804b0fa7eff35de4664bf62ff5
Fran Piura <luis.piura@kenility.com>
2023-10-03 16:26:09 -0600

    0.0.18

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "TruvideoSdkMedia",
-            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.17/TruvideoSdkMedia.xcframework.zip",
-            checksum: "9078f92a6a96be165ba415ec41bc0b5b8d6c1cbf301648c2b8b67f8b4ad4c971"
+            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.18/TruvideoSdkMedia.xcframework.zip",
+            checksum: "c87e7e06933546c4f3a4965342a72602a2dc823347fa64e0d3e6afedcd2e6344"
         ),
         .target(
             name: "TruvideoSdkMediaTargets",
