b90812805f7ecca67f4c38f688cac666a3ca6653
Fran Piura <luis.piura@kenility.com>
2023-10-03 07:39:55 -0600

    0.0.12

--- Package.swift
+++ Package.swift
@@ -21,19 +21,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "TruvideoSdkMedia",
-            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.11/TruvideoSdkMedia.xcframework.zip",
+            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.12/TruvideoSdkMedia.xcframework.zip",
             checksum: "ca2bfae5dcc96431eb216ada2fc239d2f6090fc9972ba46f2d94f1487ac30ac0"
-        ),
-        .binaryTarget(
-            name: "Shared",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-core/releases/download/0.0.18/shared.xcframework.zip",
-            checksum: "4445be410f9722f25088b0ff1b43af28788a19a286ab18b51e856ac25d92b6a6"
         ),
         .target(
             name: "TruvideoSdkMediaTargets",
             dependencies: [
                 .target(name: "TruvideoSdkMedia"),
-                .target(name: "Shared"),
                 .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
                 .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
