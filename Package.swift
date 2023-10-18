08a3cb6530c7d230dea0a78701a688bb5b266caa
Fran Piura <luis.piura@kenility.com>
2023-10-18 15:31:22 -0600

    0.0.32

--- Package.swift
+++ Package.swift
@@ -4,34 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "TruvideoSdkMedia",
+    name: "TruvideoSdkNoiseCancelling",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TruvideoSdkMedia",
-            targets: ["TruvideoSdkMediaTargets"]),
+            name: "TruvideoSdkNoiseCancelling",
+            targets: ["TruvideoSdkNoiseCancellingTargets"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", exact: "2.33.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "TruvideoSdkMedia",
-            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.31/TruvideoSdkMedia.xcframework.zip",
-            checksum: "abd91b344b2477a34f7857415f5ea2094fdd3c268ccb55f1d89f861fdf224ee6"
+            name: "TruvideoSdkNoiseCancelling",
+            url: "https://github.com/luis-piura-kenility/binary-distribution-test/releases/download/0.0.32/TruvideoSdkNoiseCancelling.xcframework.zip",
+            checksum: "17e5c04c18ae9a5a765fcf9ff82176124bb542d8cea4260e4152c3534f6cd948"
         ),
         .target(
-            name: "TruvideoSdkMediaTargets",
+            name: "TruvideoSdkNoiseCancellingTargets",
             dependencies: [
-                .target(name: "TruvideoSdkMedia"),
-                .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
-                .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
-                .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm"),
-                .product(name: "AWSCognitoIdentityProviderASF", package: "aws-sdk-ios-spm")
+                .target(name: "TruvideoSdkNoiseCancelling"),
             ],
             path: "Sources"
         ),
